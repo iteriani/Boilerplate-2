cd9fe6d1766f8567e8d38f0ad1f07d72462a8de3
iteriani <fortrex@gmail.com>
2014-04-24 15:15:14 -0700

    hey

--- public/js/pie-chart.js
+++ public/js/pie-chart.js
@@ -1,4 +1,3 @@
-
 var width = 600,
     height = 350,
     radius = Math.min(width, height) / 2;
@@ -16,44 +15,40 @@ var arc = d3.svg.arc()
 var svg = d3.select("#piechart").append("svg")
     .attr("width", width)
     .attr("height", height)
-  	.append("g")
+    .append("g")
     .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")")
     .on("click", function(){
-		$("#piechart").slideToggle();
-		$("#graph").slideToggle()
+    $("#piechart").slideToggle();
+    $("#graph").slideToggle()
     })
     .style("cursor", "pointer");
 
- 	var data = [{"init" : 100, "second" : 54325}, {"init" : 0, "second" : 50000}];
- 	var path = svg.datum(data).selectAll("path")
+svg.append("g")
+  .attr("class", "labels");
+
+svg.append("g")
+  .attr("class", "lines");
+
+  var data = [{"init" : 100, "second" : 54325}, {"init" : 0, "second" : 50000}];
+  var path = svg.datum(data).selectAll("path")
       .data(pie)
     .enter().append("path")
       .attr("fill", function(d, i) { 
-      	return color(i); 
+        return color(i); 
       })
       .attr("d", arc)
       .each(function(d) { this._current = d; }); // store the initial angles
 
 
   function switchVals(valset){
-  	console.log(valset);
-  	var i = 0;
-  	pie.value(function(d) {
+    console.log(valset);
+    var i = 0;
+    pie.value(function(d) {
      return valset[i++]; 
      }); // change the value function
     path = path.data(pie); // compute the new angles
     path.transition().duration(750).attrTween("d", arcTween); // redraw the arcs
   }
-/*
-  setInterval(function(){
-  	var data = [];
-  	for(var i = 0; i < 6; i++){
-  		data.push(parseInt(Math.random()*75000));
-  	}
-  	switchVals(data)
-  	
-  },1050)*/
-
 
 // Store the displayed angles in _current.
 // Then, interpolate from _current to the new angles.
@@ -65,3 +60,43 @@ function arcTween(a) {
     return arc(i(t));
   };
 }
+var key = function(d){ return "test"; };
+
+var text = svg.select(".labels").selectAll("text")
+    .data(pie(data), key);
+
+  text.enter()
+    .append("text")
+    .attr("dy", ".35em")
+    .text(function(d) {
+      return "test";
+    });
+  
+  function midAngle(d){
+    return d.startAngle + (d.endAngle - d.startAngle)/2;
+  }
+
+  text.transition().duration(1000)
+    .attrTween("transform", function(d) {
+      this._current = this._current || d;
+      var interpolate = d3.interpolate(this._current, d);
+      this._current = interpolate(0);
+      return function(t) {
+        var d2 = interpolate(t);
+        var pos = outerArc.centroid(d2);
+        pos[0] = radius * (midAngle(d2) < Math.PI ? 1 : -1);
+        return "translate("+ pos +")";
+      };
+    })
+    .styleTween("text-anchor", function(d){
+      this._current = this._current || d;
+      var interpolate = d3.interpolate(this._current, d);
+      this._current = interpolate(0);
+      return function(t) {
+        var d2 = interpolate(t);
+        return midAngle(d2) < Math.PI ? "start":"end";
+      };
+    });
+
+  text.exit()
+    .remove();
