7fec781b23c5c25f238893477887982abc18cab3
iteriani <fortrex@gmail.com>
2014-04-24 14:38:29 -0700

    k

--- public/js/pie-chart.js
+++ public/js/pie-chart.js
@@ -13,10 +13,6 @@ var arc = d3.svg.arc()
     .innerRadius(radius - 100)
     .outerRadius(radius - 20);
 
-var outerArc = d3.svg.arc()
-  .innerRadius(radius * 0.9)
-  .outerRadius(radius * 0.9);
-
 var svg = d3.select("#piechart").append("svg")
     .attr("width", width)
     .attr("height", height)
@@ -27,14 +23,6 @@ var svg = d3.select("#piechart").append("svg")
 		$("#graph").slideToggle()
     })
     .style("cursor", "pointer");
-    
-svg.append("g")
-  .attr("class", "slices");
-svg.append("g")
-  .attr("class", "labels");
-svg.append("g")
-  .attr("class", "lines");
-
 
  	var data = [{"init" : 100, "second" : 54325}, {"init" : 0, "second" : 50000}];
  	var path = svg.datum(data).selectAll("path")
@@ -44,71 +32,10 @@ svg.append("g")
       	return color(i); 
       })
       .attr("d", arc)
-      .each(function(d) { this._current = d; }); // store the initial angles
-
-
-  var text = svg.select(".labels").selectAll("text")
-    .data(pie(data), key);
-
-  text.enter()
-    .append("text")
-    .attr("dy", ".35em")
-    .text(function(d) {
-      return d.data.label;
-    });
-  
-  function midAngle(d){
-    return d.startAngle + (d.endAngle - d.startAngle)/2;
-  }
-
-  text.transition().duration(1000)
-    .attrTween("transform", function(d) {
-      this._current = this._current || d;
-      var interpolate = d3.interpolate(this._current, d);
-      this._current = interpolate(0);
-      return function(t) {
-        var d2 = interpolate(t);
-        var pos = outerArc.centroid(d2);
-        pos[0] = radius * (midAngle(d2) < Math.PI ? 1 : -1);
-        return "translate("+ pos +")";
-      };
-    })
-    .styleTween("text-anchor", function(d){
-      this._current = this._current || d;
-      var interpolate = d3.interpolate(this._current, d);
-      this._current = interpolate(0);
-      return function(t) {
-        var d2 = interpolate(t);
-        return midAngle(d2) < Math.PI ? "start":"end";
-      };
-    });
-
-  text.exit()
-    .remove();
-
-  /* ------- SLICE TO TEXT POLYLINES -------*/
-
-  var polyline = svg.select(".lines").selectAll("polyline")
-    .data(pie(data), key);
-  
-  polyline.enter()
-    .append("polyline");
-
-  polyline.transition().duration(1000)
-    .attrTween("points", function(d){
-      this._current = this._current || d;
-      var interpolate = d3.interpolate(this._current, d);
-      this._current = interpolate(0);
-      return function(t) {
-        var d2 = interpolate(t);
-        var pos = outerArc.centroid(d2);
-        pos[0] = radius * 0.95 * (midAngle(d2) < Math.PI ? 1 : -1);
-        return [arc.centroid(d2), outerArc.centroid(d2), pos];
-      };      
-    });
-  
-  polyline.exit()
-    .remove();
+      .each(function(d) { this._current = d; })
+      .selectAll("text")
+      .data(function(d,i){return "TEST"})
+      .enter().append("text").attr("text", function(d){return "TST"});
 
 
   function switchVals(valset){
@@ -120,6 +47,16 @@ svg.append("g")
     path = path.data(pie); // compute the new angles
     path.transition().duration(750).attrTween("d", arcTween); // redraw the arcs
   }
+/*
+  setInterval(function(){
+  	var data = [];
+  	for(var i = 0; i < 6; i++){
+  		data.push(parseInt(Math.random()*75000));
+  	}
+  	switchVals(data)
+  	
+  },1050)*/
+
 
 // Store the displayed angles in _current.
 // Then, interpolate from _current to the new angles.
